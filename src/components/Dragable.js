98e4d11270288e116fe49fe3926f27f99a6b8f91
snobojohan <johan.hallberg@gmail.com>
2017-10-24 11:57:38 +0200

    scale coins on drag

--- src/components/Dragable.js
+++ src/components/Dragable.js
@@ -7,7 +7,11 @@ import '../Dragula.css';
 export default class Dragable extends React.Component {
 
   componentDidMount() {
+
     Dragula([document.getElementById("bank"), document.getElementById("proposal-1"), document.getElementById("proposal-2"), document.getElementById("proposal-3"), document.getElementById("proposal-4"), , document.getElementById("proposal-5")]);
+
+    window.addEventListener( 'touchmove', function() {})
+
   }
 
   render() {
@@ -15,18 +19,16 @@ export default class Dragable extends React.Component {
     return (
       <div className="dragableWrapper">
 
-        <div id="bank-text" className="banktext"><p>Dra pengar till de förslag du tycker om. Men tänk på att budgeten är begränsad.</p></div>
+        <div id="bank-text" className="banktext"><p>Dra pengar till de förslag du tycker om. Men tänk på att budgeten är begränsad!</p></div>
         <div id="bank" className="container dragable-sort">
-          <div><img src="./coin.png" className="coin" /></div>
-          <div><img src="./coin.png" className="coin" /></div>
-          <div><img src="./coin.png" className="coin" /></div>
-          <div><img src="./coin.png" className="coin" /></div>
-          <div><img src="./coin.png" className="coin" /></div>
-          <div><img src="./coin.png" className="coin" /></div>
-          <div><img src="./coin.png" className="coin" /></div>
-          <div><img src="./coin.png" className="coin" /></div>
-          <div><img src="./coin.png" className="coin" /></div>
-          <div><img src="./coin.png" className="coin" /></div>
+
+          <div className="coinHolder"><img src="./coin.png" className="coin" /></div>
+          <div className="coinHolder"><img src="./coin.png" className="coin" /></div>
+          <div className="coinHolder"><img src="./coin.png" className="coin" /></div>
+          <div className="coinHolder"><img src="./coin.png" className="coin" /></div>
+          <div className="coinHolder"><img src="./coin.png" className="coin" /></div>
+          <div className="coinHolder"><img src="./coin.png" className="coin" /></div>
+
         </div>
 
         <div id="proposal-1-text" className="proposaltext"><p>Kraftigt utbyggd tunnelbana</p></div>
@@ -53,6 +55,38 @@ export default class Dragable extends React.Component {
         <div id="proposal-5" className="container dragable-sort">
 
         </div>
+
+        {/*
+        <div id="proposal-1-text" className="proposaltext"><p>Kraftigt utbyggd tunnelbana</p></div>
+        <div id="proposal-1" className="container dragable-sort">
+          <div><img src="./coin.png" className="coin" /></div>
+          <div><img src="./coin.png" className="coin" /></div>
+        </div>
+
+        <div id="proposal-2-text" className="proposaltext"><p>Alla över 85 som vill ska få plats på servicehus</p></div>
+        <div id="proposal-2" className="container dragable-sort">
+          <div><img src="./coin.png" className="coin" /></div>
+          <div><img src="./coin.png" className="coin" /></div>
+        </div>
+
+        <div id="proposal-3-text" className="proposaltext"><p>Fler och bättre bibliotek, nya scener och en växande barnkultur</p></div>
+        <div id="proposal-3" className="container dragable-sort">
+          <div><img src="./coin.png" className="coin" /></div>
+          <div><img src="./coin.png" className="coin" /></div>
+        </div>
+
+        <div id="proposal-4-text" className="proposaltext"><p>Lys upp utemiljön och ha fler synliga poliser och kameror</p></div>
+        <div id="proposal-4" className="container dragable-sort">
+          <div><img src="./coin.png" className="coin" /></div>
+          <div><img src="./coin.png" className="coin" /></div>
+        </div>
+
+        <div id="proposal-5-text" className="proposaltext"><p>Giftfri stad 2030</p></div>
+        <div id="proposal-5" className="container dragable-sort">
+          <div><img src="./coin.png" className="coin" /></div>
+          <div><img src="./coin.png" className="coin" /></div>
+        </div>
+        */}
       </div>
     );
   }
